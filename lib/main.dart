880bf391b629b106aee38fd22a4c810241432f2c
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-09-25 15:47:44 +0330

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coffee_animation/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Coffee Animation',
+      theme: ThemeData(),
+      home: const SplashScreen(),
+    );
+  }
+}
